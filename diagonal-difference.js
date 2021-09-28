5038a82fea2976913bef68e3d0a01006bba90804
renjval12 <reneejoyvalentine3@gmail.com>
2021-09-28 18:43:59 -0400

    Update diagonal-difference.js

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -1,16 +1,15 @@
 
-
-function diagonalDifference() {
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-    let len = arr.length;
-    let leftDiagSum = 0;
-    let rightDiagSum = 0;
-    let difference = 0
+let leftDiagSum = 0;
+let rightDiagSum = 0;
+let difference = 0
+function diagonalDifference(arr) {
+    let len = arr[0].length;
+    
     for (let i = 0, j = len - 1; i < len; i++, j--) {
         leftDiagSum += arr[i][i]
         rightDiagSum += arr[i][j];
-        difference = leftDiagSum - rightDiagSum
+    difference = leftDiagSum - rightDiagSum
+
     }
     return Math.abs(difference)
 }
